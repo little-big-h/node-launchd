f695cb2960b028393e676cfb194390fbd69b2ea1
George Nassas <George+Development@Nassas.com>
2016-01-16 08:49:33 -0500

    Use newer style V8 programming, compatible with Node 0.11+

--- checkin.cc
+++ checkin.cc
@@ -5,63 +5,69 @@
 #include <sys/event.h>
 #include <fstream>
 
-using namespace v8;
+//using namespace v8;
+using v8::Exception;
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Number;
+using v8::Object;
+using v8::String;
+using v8::Value;
 
-Handle<Value> Method(const Arguments& args) {
-	HandleScope scope;
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
 	if (args.Length() != 1) {
-    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-    return scope.Close(Undefined());
+    isolate->ThrowException(Exception::TypeError(
+                                String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
 	}
+
 	char* handleName = strdup(std::string(*v8::String::Utf8Value(args[0]->ToString())).c_str());
 
-	int result = -1;
 	launch_data_t checkinRequest = launch_data_new_string(LAUNCH_KEY_CHECKIN);
 	launch_data_t checkinResponse = launch_msg(checkinRequest);
-    
+
 	switch (launch_data_get_type(checkinResponse)) {
 	case LAUNCH_DATA_ERRNO: {
 		int error = launch_data_get_errno(checkinResponse);
 		char message[128];
 		snprintf(message, 128, "Launchd data error %d" , error);
-		ThrowException(Exception::Error(String::New(message)));
-		return scope.Close(Undefined());
+		isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, message)));
 		break;
 	}
 
 	case LAUNCH_DATA_DICTIONARY: {
 		launch_data_t sockets_dict = launch_data_dict_lookup(checkinResponse, LAUNCH_JOBKEY_SOCKETS);
 		if (NULL == sockets_dict) {
-			ThrowException(Exception::Error(String::New("No sockets found to answer requests on!")));
-			return scope.Close(Undefined());
+      isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "No sockets found to answer requests on!")));
 		} else if (launch_data_dict_get_count(sockets_dict) > 1) {
-			ThrowException(Exception::Error(String::New("Some sockets will be ignored!")));
-			return scope.Close(Undefined());
-		}
+			isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Some sockets will be ignored!")));
+		} else {
 
-		launch_data_t listening_fd_array = launch_data_dict_lookup(sockets_dict, handleName);
-		if (listening_fd_array == NULL) {
-			ThrowException(Exception::Error(String::New("No known sockets found to answer requests on!")));
-			return scope.Close(Undefined());
-		}
-
-		launch_data_t this_listening_fd = launch_data_array_get_index(listening_fd_array, 0);
-		result = launch_data_get_fd(this_listening_fd);
+		  launch_data_t listening_fd_array = launch_data_dict_lookup(sockets_dict, handleName);
+		  if (listening_fd_array == NULL) {
+			  isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "No known sockets found to answer requests on!")));
+		  } else {
+		    launch_data_t this_listening_fd = launch_data_array_get_index(listening_fd_array, 0);
+		    int fd = launch_data_get_fd(this_listening_fd);
+        Local<Number> port = Number::New(isolate, fd);
+        args.GetReturnValue().Set(port);
+      }
+    }
 		break;
 	}
 	default: {
-		ThrowException(Exception::Error(String::New("Unknown Error")));
-		return scope.Close(Undefined());
+    isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Unknown Error")));
 		break;
 	}
 	}
 	free(handleName);
-	return scope.Close(Integer::New(result));
 }
 
-void init(Handle<Object> exports) {
-	exports->Set(String::NewSymbol("getSocketFileDescriptorForName"),
-							 FunctionTemplate::New(Method)->GetFunction());
+void init(Local<Object> exports) {
+	NODE_SET_METHOD(exports, "getSocketFileDescriptorForName", Method);
 }
 
 NODE_MODULE(checkin, init)
